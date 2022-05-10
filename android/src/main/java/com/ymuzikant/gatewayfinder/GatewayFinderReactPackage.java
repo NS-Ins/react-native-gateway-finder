307c82e83a0bd16344c5ae70e022ea8d5fc21efa
NS-Ins <99880350+NS-Ins@users.noreply.github.com>
2022-05-10 10:29:28 +0530

    Commented the override.

--- android/src/main/java/com/ymuzikant/gatewayfinder/GatewayFinderReactPackage.java
+++ android/src/main/java/com/ymuzikant/gatewayfinder/GatewayFinderReactPackage.java
@@ -24,7 +24,7 @@ public class GatewayFinderReactPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
